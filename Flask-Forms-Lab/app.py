202f183d684992688ea88a0e024992d740cc826c
nameerja23-meet <nemojabara@gmail.com>
2022-07-28 12:07:28 +0300

    Initial commit

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -6,19 +6,36 @@ app = Flask(  # Create a flask app
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
+app.config['SECRET_KEY'] = 'shh'
 
 
-username = "llo2ay"
+username = "Nameer"
 password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods =['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'POST':
+		inpUsername = request.form['username']
+		inpPassword = request.form['password']
+		if (inpUsername == username) and (inpPassword == password):
+			return redirect(url_for('home'))
+		else:
+			return render_template('login.html', error = "Invalid username or password, try again.")
+	else:
+		return render_template('login.html')
+	
   
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	return render_template('home.html',
+		friends=facebook_friends)
 
 
+@app.route('/friend_exists/<string:name>', methods=['GET', 'POST'])
+def friendsExists():
+	return render_template('friend_exists.html')
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
